eb19f45927343f2de2d83fa32dcc64e82e070154
Marat Suyargulov <ml65@rambler.ru>
2022-05-14 20:35:25 +0300

    initial

---
+++ api/protected/config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// uncomment the following lines to use a MySQL database
+	
+	'connectionString' => 'mysql:host=172.18.0.1;dbname=ml65_spb_ru',
+	'emulatePrepare' => true,
+	'username' => 'ml65',
+	'password' => 'xjwOL])i6]Q!wqnr',
+	'charset' => 'utf8',
+	
+);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,27 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '1.1.x-dev',
+        ),
+        'reference' => NULL,
+        'name' => 'yiisoft/yii',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'yiisoft/yii' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '1.1.x-dev',
+            ),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
